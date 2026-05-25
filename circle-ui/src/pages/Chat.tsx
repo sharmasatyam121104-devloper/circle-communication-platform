979b83a040eed78ae79fb004f59bd76c9b38a1b2
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-05-25 13:48:28 +0530

    working on caht ui 2nd changes

--- circle-ui/src/pages/Chat.tsx
+++ circle-ui/src/pages/Chat.tsx
@@ -9,10 +9,13 @@ import SenderMessage from "../Components/chats/SenderMessage"
 import ReceiverMessage from "../Components/chats/ReciverMessage"
 import { MdOutlineVideoCall } from "react-icons/md"
 import ChatMemberCard from "../Components/chats/ChatMemberCard"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 
 const Chat = () => {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const isChatOpen = Boolean(id); 
+
 
   return (
     <div className="h-screen  flex bg-indigo-300 p-2">
@@ -40,51 +43,77 @@ const Chat = () => {
 
       </div>
       <div className="w-9/12 bg-gray-600 rounded-2xl m-2">
-        <div className="flex items-center justify-between gap-1 w-full bg-white rounded-t-2xl px-6 py-1">
-          <Link to={'/chat'} className="block lg:hidden"><ArrowBigLeft/></Link>
-          <div className="flex gap-2">
-            <Avatar/>
-            <div>
-              <h1 className="font-medium">dummy@gmail.com</h1>
-              <p className="text-sm">dummy name</p>
-            </div>
-          </div>
-
-          <div className="flex gap-5 justify-center items-center mr-8">
-            <h1 className="text-4xl hover:text-green-400 active:scale-75 cursor-pointer"><MdOutlineVideoCall /></h1>
-            <h1 className="text-3xl hover:text-green-400 active:scale-75 cursor-pointer"><IoCallOutline /></h1>
-          </div>
-        </div>
-
-        <div className="h-140 overflow-y-auto">
-            <SenderMessage
-              message="Bhai ye project report dekh"
-              time="9:12 PM"
-              isSeen={true}
-              avatar="https://i.pravatar.cc/150?img=5"
-              attachment={{
-                fileName: "MERN_Project_Report.pdf",
-                fileSize: "2.4 MB",
-                fileType: "PDF",
-                fileUrl: "/files/report.pdf",
-              }}
-            />
-            <ReceiverMessage
-              message="Bhai message receive ho gaya"
-              time="9:12 PM"
-              isSeen={true}
-            />
+        {
+          isChatOpen === false 
+          ?
+          <div className="h-full flex flex-col justify-center items-center text-white px-6 text-center">
             
-        </div>
-        <div className="flex items-center h-16 mb-8">
-          <div className="bg-white ml-6 rounded-full p-2 active:scale-95 cursor-pointer">
-            <CgAttachment className="text-4xl text-indigo-600 "/>
+            <div className="bg-indigo-500 p-5 rounded-full shadow-lg mb-6">
+              <MessageCircleDashed size={50} />
+            </div>
+
+            <h1 className="text-3xl font-bold mb-3">
+              Welcome to Circle Chat
+            </h1>
+
+            <p className="text-gray-300 max-w-md leading-relaxed">
+              Select a conversation from the left sidebar to start chatting
+              with your friends and team members.
+            </p>
+
+            <p className="text-sm text-gray-400 mt-4">
+              Send messages, share files, and stay connected in real-time.
+            </p>
           </div>
-          <Input height="h-17 mb-2 ml-6" placeholder="Write your message here..."/>
-          <div className="bg-indigo-600 ml-6 rounded-full p-4 active:scale-95 cursor-pointer h-fit w-fit flex items-center justify-center mr-8 hover:bg-green-600">
-             <ArrowUpRight  size={27} className=" text-white"/>
-          </div>
-        </div>
+          :
+          <>
+            <div className="flex items-center justify-between gap-1 w-full bg-white rounded-t-2xl px-6 py-1">
+              <Link to={'/chat'} className="block lg:hidden"><ArrowBigLeft/></Link>
+              <div className="flex gap-2">
+                <Avatar/>
+                <div>
+                  <h1 className="font-medium">dummy@gmail.com</h1>
+                  <p className="text-sm">dummy name</p>
+                </div>
+              </div>
+
+              <div className="flex gap-5 justify-center items-center mr-8">
+                <h1 className="text-4xl hover:text-green-400 active:scale-75 cursor-pointer"><MdOutlineVideoCall /></h1>
+                <h1 className="text-3xl hover:text-green-400 active:scale-75 cursor-pointer"><IoCallOutline /></h1>
+              </div>
+            </div>
+
+            <div className="h-140 overflow-y-auto">
+                <SenderMessage
+                  message="Bhai ye project report dekh"
+                  time="9:12 PM"
+                  isSeen={true}
+                  avatar="https://i.pravatar.cc/150?img=5"
+                  attachment={{
+                    fileName: "MERN_Project_Report.pdf",
+                    fileSize: "2.4 MB",
+                    fileType: "PDF",
+                    fileUrl: "/files/report.pdf",
+                  }}
+                />
+                <ReceiverMessage
+                  message="Bhai message receive ho gaya"
+                  time="9:12 PM"
+                  isSeen={true}
+                />
+                
+            </div>
+            <div className="flex items-center h-16 mb-8">
+              <div className="bg-white ml-6 rounded-full p-2 active:scale-95 cursor-pointer">
+                <CgAttachment className="text-4xl text-indigo-600 "/>
+              </div>
+              <Input height="h-17 mb-2 ml-6" placeholder="Write your message here..."/>
+              <div className="bg-indigo-600 ml-6 rounded-full p-4 active:scale-95 cursor-pointer h-fit w-fit flex items-center justify-center mr-8 hover:bg-green-600">
+                <ArrowUpRight  size={27} className=" text-white"/>
+              </div>
+            </div>
+          </>
+        }
       </div>
     </div>
   )
