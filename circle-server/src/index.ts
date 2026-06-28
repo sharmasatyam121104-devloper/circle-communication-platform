9bdb3ba787f42b8f50f03e37ebf49218b2e437f3
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-06-28 17:02:23 +0530

    change port

--- circle-server/src/index.ts
+++ circle-server/src/index.ts
@@ -10,7 +10,7 @@ connectDb();
 
 import "./config/redis.config";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
