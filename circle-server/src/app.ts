efc2042647bb26ef8ab1c7cf7a132c5dd64819aa
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-06-28 13:15:42 +0530

    feat(ci): automate Docker Compose deployment to EC2 2

--- circle-server/src/app.ts
+++ circle-server/src/app.ts
@@ -39,7 +39,7 @@ app.get("/test", (req, res) => {
   res.send("Server Running...");
 });
 
-app.get("/test-v4", (req, res) => {
+app.get("/test-v5", (req, res) => {
   res.send("Server Running...");
 });
 
